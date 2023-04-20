732e946de8735f69d1a627be75521bba15e04d00
Tae Kim <taekim.dev@gmail.com>
2023-04-20 22:31:57 +0800

    Adds Sliders for Rating and Prices

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,12 +15,75 @@ function HomePage() {
           <span className="coin-balance">100</span>
         </div>
       </header>
-      {/* Add the Left Pane and Right Pane components here */}
-      <div className="left-pane">
-        {/* Add the Left Pane content here */}
-      </div>
-      <div className="right-pane">
-        {/* Add the Right Pane content here */}
+      <div className="panes-container">
+        <div className="left-pane">
+          <h2 className="category-title">Category</h2>
+          <select className="category-dropdown">
+            <option value="all">All</option>
+            {/* Add more options for different categories */}
+          </select>
+          <h2 className="rating-title">Rating</h2>
+          {/* Add the rating range slider component */}
+          <input
+            type="range"
+            min="3"
+            max="5"
+            defaultValue="3"
+            className="rating-range"
+          />
+          <div className="rating-labels">
+            <span className="rating-label">★3</span>
+            <span className="rating-label">★4</span>
+            <span className="rating-label">★5</span>
+          </div>
+          <h2 className="prices-title">Prices</h2>
+          {/* Add the prices range slider component */}
+          <div className="prices-range-container">
+            <input
+              type="number"
+              min="0"
+              max="100"
+              defaultValue="0"
+              className="min-price-input"
+            />
+            <input
+              type="range"
+              min="0"
+              max="100"
+              defaultValue="0"
+              className="price-range"
+            />
+            <input
+              type="number"
+              min="0"
+              max="100"
+              defaultValue="100"
+              className="max-price-input"
+            />
+          </div>
+          <div className="checkbox-container">
+            <label className="checkbox-label">
+              <input type="checkbox" className="discount-checkbox" />
+              Discount
+            </label>
+            <label className="checkbox-label">
+              <input type="checkbox" className="in-stock-checkbox" />
+              In Stock
+            </label>
+          </div>
+        </div>
+        <div className="right-pane">
+          <div className="sorting-container">
+            <span className="sorted-by-text">Sorted by</span>
+            <button className="sort-button top-rated active">
+              Top Rated
+            </button>
+            <button className="sort-button economy">Economy</button>
+            <button className="sort-button luxury">Luxury</button>
+          </div>
+          {/* Add the product grid component */}
+          {/* Add the pagination component */}
+        </div>
       </div>
     </div>
   );
