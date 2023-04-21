44571ddab4d5e9912bd5fb9edad39753be0e3495
Tae Kim <taekim.dev@gmail.com>
2023-04-21 23:45:19 +0800

    Balance context for Landing Page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../assets/styles/LandingPage.css';
 import logo from '../assets/images/logo.png';
 import coinIcon from '../assets/images/coin-icon.png';
+import AppContext from '../context/AppContext';
 
 function LandingPage(){
+  const {coinBalance} = useContext(AppContext);
+
   return (
     <div className="landing-page">
       <img src={logo} alt="Logo" className="logo" />
@@ -12,7 +15,7 @@ function LandingPage(){
       <button className="enter-button">Enter</button>
       <div className="coin-info">
         <img src={coinIcon} alt="Coin" className="coin-icon" />
-        <span className="coin-text">x100</span>
+        <span className="coin-text">x{coinBalance}</span>
       </div>
     </div>
   );
